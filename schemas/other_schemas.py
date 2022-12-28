7b6f0282598268ad3496c4d6cfade8abdd9737bd
Jack Kazanjyan <jack4ac22@icloud.com>
2022-12-28 09:10:54 +0100

    adjust Token' schema

--- schemas/other_schemas.py
+++ schemas/other_schemas.py
@@ -2,10 +2,7 @@ from pydantic import BaseModel
 
 
 class Token(BaseModel):
-    access_token: str
-    token_type: str
+    access_token: str ="eyJhbGciOiqIUz1NaiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiamFtZSI6IkavaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQasw5c"
+    token_type: str ="Bearer"
 
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
